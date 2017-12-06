8aaaec13b7c5d88db232679fec3f054044f0eb0e
IamBusy <1342247033@qq.com>
2017-12-06 22:54:51 +0800

    Finish

--- 36 Valid Sudoku.cpp
+++ 36 Valid Sudoku.cpp
@@ -7,3 +7,39 @@
 //
 
 #include <stdio.h>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        const int boardSize = 9;
+        const char MARK = '.';
+        int rows[boardSize][boardSize] = {0};
+        int columns[boardSize][boardSize] = {0};
+        int blocks[boardSize][boardSize] = {0};
+        for (int i=0; i<boardSize; i++) {
+            for (int j=0; j<boardSize; j++) {
+                if(board[i][j] != MARK) {
+                    int num = board[i][j] - '1';
+                    if(rows[i][num] != 0) {
+                        return false;
+                    }
+                    rows[i][num]++;
+                    if(columns[j][num] != 0) {
+                        return false;
+                    }
+                    columns[j][num]++;
+                    
+                    if(blocks[i/3 * 3+j/3][num] != 0) {
+                        return false;
+                    }
+                    blocks[i/3 * 3+j/3][num] ++;
+                }
+            }
+        }
+        return true;
+    }
+};
+
