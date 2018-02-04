a91ab1ccecec7ab643c6c226e248bb35d0ac7458
IamBusy <1342247033@qq.com>
2018-02-04 21:38:25 +0800

    Reduce space cost

--- 62 Unique Paths.cpp
+++ 62 Unique Paths.cpp
@@ -14,20 +14,16 @@ using namespace std;
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int **paths = new int*[m];
-        for (int i=0; i<m; i++) {
-            paths[i] = new int[n];
-            paths[i][0] = 1;
-        }
+        int *paths = new int[n];
         for (int i=0; i<n; i++) {
-            paths[0][i] = 1;
+            paths[i] = 1;
         }
         for (int i=1; i<m; i++) {
             for (int j=1; j<n; j++) {
-                paths[i][j] = paths[i][j-1] + paths[i-1][j];
+                paths[j] = paths[j-1] + paths[j];
             }
         }
-        return paths[m-1][n-1];
+        return paths[n-1];
     }
 };
 
---
+++ 63 Unique Paths II.cpp
@@ -0,0 +1,9 @@
+//
+//  63 Unique Paths II.cpp
+//  leetcode
+//
+//  Created by william wei on 04/02/2018.
+//  Copyright © 2018 william wei. All rights reserved.
+//
+
+#include <stdio.h>
