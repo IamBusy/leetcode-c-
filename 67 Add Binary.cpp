048a5d564229fd0187b3cac129964d9663db0270
IamBusy <1342247033@qq.com>
2018-02-05 13:45:55 +0800

    Finish

--- 67 Add Binary.cpp
+++ 67 Add Binary.cpp
@@ -7,3 +7,30 @@
 //
 
 #include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        if (a.length() < b.length()) {
+            swap(a, b);
+        }
+        int carry = 0;
+        for (int i = 0; i<b.length(); i++) {
+            int sum = carry + a[a.length()-1-i] - '0' + b[b.length()-1-i] - '0';
+            carry = sum / 2;
+            a[a.length()-1-i] = sum % 2;
+        }
+        for (int i= b.length(); i<a.length() && carry; i++) {
+            int sum = a[a.length()-1-i] - '0' + carry;
+            carry = sum/2;
+            a[a.length()-1-i] == sum % 2;
+        }
+        if (carry) {
+            a = char(carry + '0') + a;
+        }
+        return a;
+    }
+};
