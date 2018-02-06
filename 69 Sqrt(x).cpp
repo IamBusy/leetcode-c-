56e4320699c68a82c257e5fc82d1ea19438aab8a
IamBusy <1342247033@qq.com>
2018-02-06 15:23:41 +0800

    Add new method

--- 69 Sqrt(x).cpp
+++ 69 Sqrt(x).cpp
@@ -10,21 +10,32 @@
 
 class Solution {
 public:
+    // Newton-Raphson method
     int mySqrt(int x) {
         if(!x) return 0;
-        int low = 1, high = x;
-        while (true) {
-            int mid = (low + high) / 2;
-            if (x / mid < mid) {
-                high = mid;
-            } else if (x / mid == mid) {
-                return mid;
-            } else if(x / (mid + 1) < mid + 1) {
-                return mid;
-            } else {
-                low = mid;
-            }
-            
+        long mid = x / 2 + 1;
+        while (mid*mid > x) {
+            mid = (mid + x /mid)/2;
         }
+        return mid;
     }
+//    // Binary search
+//    int mySqrt(int x) {
+//        if(!x) return 0;
+//        int low = 1, high = x;
+//        while (true) {
+//            int mid = (low + high) / 2;
+//            if (x / mid < mid) {
+//                high = mid;
+//            } else if (x / mid == mid) {
+//                return mid;
+//            } else if(x / (mid + 1) < mid + 1) {
+//                return mid;
+//            } else {
+//                low = mid;
+//            }
+//
+//        }
+//    }
+    
 };
