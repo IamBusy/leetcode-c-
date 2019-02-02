2fbe787723366a6791c3a9d2b1dd2e1a49caa09b
william <1342247033@qq.com>
2019-02-02 13:46:56 +0800

    Update

--- 81 Search in Rotated Sorted Array II.cpp
+++ 81 Search in Rotated Sorted Array II.cpp
@@ -11,22 +11,29 @@
 
 using namespace std;
 
+
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         int start = 0, end = nums.size()-1, mid;
-        while (start < end) {
+        if (nums.size() == 0) {
+            return false;
+        }
+        while (start <= end) {
             mid  = (start + end) / 2;
             if (nums[mid] == target) {
-                return false;
-            } else if (nums[mid] > nums[end]) {
+                return true;
+            } else if (nums[mid] == nums[end] && nums[mid] == nums[start]) {
+                start++;
+                end--;
+            } else if (nums[mid] >= nums[end] && nums[mid] >= nums[start]) {
                 if (nums[mid] > target && nums[start] <= target) {
                     end = mid - 1;
                 } else {
                     start = mid + 1;
                 }
             } else {
-                if (nums[mid] < target && nums[end] > target) {
+                if (nums[mid] < target && nums[end] >= target) {
                     start = mid + 1;
                 } else {
                     end = mid - 1;
@@ -36,5 +43,4 @@ public:
         return false;
     }
 };
-
 
---
+++ 82 Remove Duplicates from Sorted List II.cpp
@@ -0,0 +1,9 @@
+//
+//  82 Remove Duplicates from Sorted List II.cpp
+//  leetcode
+//
+//  Created by william wei on 2019/2/2.
+//  Copyright © 2019 william wei. All rights reserved.
+//
+
+#include <stdio.h>
