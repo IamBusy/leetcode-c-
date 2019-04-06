d6747e2e2a33b085d69e803b4005818e7d9c6ab8
william <1342247033@qq.com>
2019-04-06 20:55:03 +0800

    Finish

--- 96 Unique Binary Search Trees.cpp
+++ 96 Unique Binary Search Trees.cpp
@@ -7,3 +7,23 @@
 //
 
 #include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int numTrees(int n) {
+        int *table = new int[max(n+1, 2)];
+        memset(table, 0, sizeof(int) * max(n+1, 2));
+        table[0] = 0;
+        table[1] = 1;
+        for (int i=2; i<=n; i++) {
+            for (int j=1; j<=i; j++) {
+                table[i] += max(table[j-1], 1) * max(table[i-j], 1);
+            }
+        }
+        return table[n];
+    }
+};
