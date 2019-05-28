51da68e66d357b09685dcfa8a0e56a09d7b94877
william <1342247033@qq.com>
2019-05-28 10:39:21 +0800

    Finish

---
+++ 99 Recover Binary Search Tree.cpp
@@ -0,0 +1,49 @@
+//
+//  99 Recover Binary Search Tree.cpp
+//  leetcode
+//
+//  Created by william wei on 2019/4/10.
+//  Copyright © 2019 william wei. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    void recoverTree(TreeNode* root) {
+        stack<TreeNode*> st;
+        TreeNode *cur = root;
+        TreeNode *first = NULL;
+        TreeNode *second = NULL;
+        TreeNode *pre = NULL;
+        while (cur || !st.empty()) {
+            while (cur) {
+                st.push(cur);
+                cur = cur->left;
+            }
+            cur = st.top();
+            st.pop();
+            // If pre is null, first will be also null.
+            if (first == NULL && (pre == NULL || pre->val >= cur->val)) {
+                first = pre;
+            }
+            
+            if (first != NULL && (pre->val >= cur->val)) {
+                second = cur;
+            }
+            pre = cur;
+            cur = cur->right;
+        }
+        swap(first->val, second->val);
+    }
+};
